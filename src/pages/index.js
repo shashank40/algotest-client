6364c069b7cee6edf34c99b10516d67f0222b9f4
shashank40 <shashanktiwari40@gmail.com>
2023-07-12 20:24:40 +0530

    Changed title of the project

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,11 +11,11 @@ export default function Home() {
     >
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full  before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-      <h1 className={`mb-3 text-4xl font-bold`}>AlgoTest Task</h1> 
+      <h1 className={`mb-3 text-4xl font-bold`}>Trading System Charting Implementation</h1> 
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <LinkComponent heading="Phase I" description="Lightweight charts (Single Instrument)" hreftag= "/phase1"/>
+        <LinkComponent heading="Phase I" description="Lightweight charts (Single Instrument Just Data Setting)" hreftag= "/phase1"/>
         <LinkComponent heading="Phase II" description="Fetch Data from websockets." hreftag= "/phase2"/>
         <LinkComponent heading="Phase III" description="OHLC(with option to change resolution)" hreftag= "/phase3"/>
         <LinkComponent heading="Phase IV" description="Multiple Instrument(OHLC + Choose data + resolution change)" hreftag= "/phase4"/>
